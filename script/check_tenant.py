8fce5a96280ee7ad59b280fe3694d7c2cc21a6b1
Chibraax <alexandrelesieur@hotmail.fr>
2026-06-20 16:43:47 +0200

    correction of check tenant

--- script/check_tenant.py
+++ script/check_tenant.py
@@ -1,10 +1,12 @@
 import sys, json, pynetbox
 
-all_tenants: list = ["StudyP", "My Tenant", "salut"]
 
 url, token = sys.argv[1], sys.argv[2]
 nb: pynetbox.api = pynetbox.api(url, token)
 
 tenants = nb.tenancy.tenants.all()
 tenant_names = [t.name for t in tenants]
+
+
+# Return all tenants in netbox
 print(json.dumps(tenant_names))
